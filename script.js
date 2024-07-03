1e5c183eb58a38c1c36d7b949f727c6378b9f5e2
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-07-03 13:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+function findDomElement(elementId) {
+	var element  = document.getElementById(elementId)
+	var level = 0
+
+	while (element) {
+		level++;
+		element = element.parentElement;
+	}
+    console.log(level)
+	return level
+}
+
+var domLevel = findDomElement('level')
+
+alert('The level of the element is: ' + domLevel);
